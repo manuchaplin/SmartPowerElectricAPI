392ef18877981e15baf433bcd3ae658a5f4564db
manuchaplin <manuchaplin@gmail.com>
2025-01-09 18:39:07 +0100

    WIP

--- SmartPowerElectricAPI/Controllers/MaterialController.cs
+++ SmartPowerElectricAPI/Controllers/MaterialController.cs
@@ -11,7 +11,7 @@ namespace SmartPowerElectricAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
     public class MaterialController : ControllerBase
     {
         private readonly IMaterialRepository _materialRepository;
